4e37e17f3ba37abab27e77d2db556f3f917822e0
CuongNC <cuongnc@gonrin.com>
2019-12-14 10:20:24 +0700

    add login view

--- static/js/app/bases/views/LoginView.js
+++ static/js/app/bases/views/LoginView.js
@@ -4,53 +4,20 @@ define(function (require) {
     var $                   = require('jquery'),
         _                   = require('underscore'),
         Gonrin            	= require('gonrin'),
-        storejs				= require('store'),
+        
         tpl                 = require('text!app/bases/tpl/login.html'),
         template = _.template(tpl);
     return Gonrin.View.extend({
         render: function () {
         	var self = this;
-        	storejs.set('X-USER-TOKEN', '');
+        	
         	self.getApp().currentUser = null;
-        	this.getApp().data("current_so", null);
             this.$el.html(template());
             
-//            var qrcode = self.getApp().getRouter().getParam("qr");
-            var qrcode = self.getApp().getParameterUrl("qr", window.location.href);
-//            console.log(window.location.href);
-//            console.log(qrcode);
-//            if(qrcode !== undefined && qrcode!== null && qrcode !==""){
-//            	this.$el.find("#msg_qrcode").html('Mã Qr "'+qrcode+'" chưa được gắn với sổ chăm sóc.');
-//            }
-            $("#recover_account").unbind('click').bind('click', function(){
-            	self.getApp().getRouter().navigate("recorver");
-        	});
-            $("#register-btn").unbind('click').bind('click', function(){
-            	if(qrcode !== undefined && qrcode!== null && qrcode !==""){
-        			self.getApp().getRouter().navigate("dangky?qr="+qrcode);
-                }else{
-                	self.getApp().getRouter().navigate("dangky");
-                }
-                
-        	});
         	$("#forget-password").unbind('click').bind('click', function(){
                 self.getApp().getRouter().navigate("forgot");
         	});
 
-        	$("#loginfacebook").unbind('click').bind('click',function(){
-                var fbLoginSuccess = function (userData) {
-                  console.log("UserInfo: ", userData);
-                  facebookConnectPlugin.getAccessToken(function(token) {
-                    console.log("Token: " + token);
-                  });
-                }
-
-                facebookConnectPlugin.login(["public_profile"], fbLoginSuccess,
-                  function (error) {
-                    console.log(error)
-                  }
-                );
-        	});
             
             this.$el.find("#login-form").unbind("submit").bind("submit", function(){
             	self.processLogin();
@@ -65,22 +32,13 @@ define(function (require) {
 			   console.log(password);
 //       		var qrcode = this.getApp().getRouter().getParam("qr");
        		var qrcode = this.getApp().getParameterUrl("qr", window.location.href);
-       		var data;
-       		if(qrcode !== undefined && qrcode!== null && qrcode !==""){
-       			data = JSON.stringify({
-       		        data: username,
-       		        password: password,
-       		        qr: qrcode
-       		    });
-       		} else {
-       			data = JSON.stringify({
-       		        data: username,
-       		        password: password
-       		    });
-       		}
+       		var data = JSON.stringify({
+       		    data: username,
+       		    password: password
+       		});
        		var self = this;
        		$.ajax({
-       		    url:  self.getApp().serviceURL+'/api/v1/login',
+       		    url:  self.getApp().serviceURL+'/user/login',
        		    type: 'post',
        		    data: data,
        		    headers: {
@@ -95,16 +53,11 @@ define(function (require) {
        		    dataType: 'json',
        		    success: function (data) {
        		    	console.log(data);
-       		    	$.ajaxSetup({
-       		    	    headers: {
-       		    	    	'X-USER-TOKEN': data.token
-       		    	    }
-       		    	});
-       		    	storejs.set('X-USER-TOKEN', data.token);
+       		    	
        		    	self.getApp().postLogin(data);
-       		    	if(password === undefined || password===""){
-       		    		self.router.navigate("user/profile");
-       		    	}
+       		    	// if(password === undefined || password===""){
+       		    	// 	self.router.navigate("user/profile");
+       		    	// }
        		    },
        		    error: function(request, textStatus, errorThrown) {
        		    	//console.log(request);
