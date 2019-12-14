01491f6c2a728948f66bf2f605149b50e354ae1b
CuongNC <cuongnc@gonrin.com>
2019-12-14 10:39:50 +0700

    add login view

--- static/js/app/bases/views/LoginView.js
+++ static/js/app/bases/views/LoginView.js
@@ -18,7 +18,7 @@ define(function (require) {
                 self.getApp().getRouter().navigate("forgot");
         	});
 
-            
+            console.log("Login view render");
             this.$el.find("#login-form").unbind("submit").bind("submit", function(){
             	self.processLogin();
             	return false;
@@ -26,6 +26,7 @@ define(function (require) {
             return this;
         },
        	processLogin: function(){
+			console.log("LoginView Process login");
        		var username = this.$('[name=username]').val();
 			   var password = this.$('[name=password]').val();
 			   console.log(name);
@@ -33,7 +34,7 @@ define(function (require) {
 //       		var qrcode = this.getApp().getRouter().getParam("qr");
        		var qrcode = this.getApp().getParameterUrl("qr", window.location.href);
        		var data = JSON.stringify({
-       		    data: username,
+       		    user_name: username,
        		    password: password
        		});
        		var self = this;
