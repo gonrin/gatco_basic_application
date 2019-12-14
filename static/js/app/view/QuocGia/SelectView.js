bbbd167bbf7c508fb6bbdf090e53631ca0454e20
CuongNC <cuongnc@gonrin.com>
2019-12-14 11:45:32 +0700

    add tinhthanh api

---
+++ static/js/app/view/QuocGia/SelectView.js
@@ -0,0 +1,57 @@
+define(function (require) {
+    "use strict";
+    var $                   = require('jquery'),
+        _                   = require('underscore'),
+        Gonrin				= require('gonrin');
+    
+    var template 			= require('text!app/view/QuocGia/tpl/collection.html'),
+    	schema 				= require('json!schema/QuocGiaSchema.json');
+
+    return Gonrin.CollectionDialogView.extend({
+    	template : template,
+    	modelSchema	: schema,
+    	urlPrefix: "/api/v1/",
+    	collectionName: "quocgia",
+    	textField: "ten",
+    	valueField: "id",
+    	tools : [
+    	    {
+    	    	name: "defaultgr",
+    	    	type: "group",
+    	    	groupClass: "toolbar-group",
+    	    	buttons: [
+					{
+		    	    	name: "select",
+		    	    	type: "button",
+		    	    	buttonClass: "btn-success btn-sm",
+		    	    	label: "TRANSLATE:SELECT",
+		    	    	command: function(){
+		    	    		var self = this;
+		    	    		self.trigger("onSelected");
+		    	    		self.close();
+		    	    	}
+		    	    },
+    	    	]
+    	    },
+    	],
+    	uiControl:{
+    		fields: [
+	    	     { field: "ma", label: "Mã", width:150},
+		     	 { field: "ten", label: "Tên", width:250 },
+		    ],
+		    onRowClick: function(event){
+	    		this.uiControl.selectedItems = event.selectedItems;
+	    		
+	    	},
+    	},
+    	render:function(){
+    		var self= this;
+    		
+    		self.applyBindings();
+    		
+    		return this;
+    	},
+    	
+    });
+
+});
